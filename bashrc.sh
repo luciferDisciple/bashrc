8807bdf0e1f27461074ed90559096c6e05183760
luciferDisciple <waizer12@gmail.com>
2024-07-20 02:29:56 +0200

    newest_file(): dont fail if no $1 and dont put '.' if not passed explicitly

--- bashrc.sh
+++ bashrc.sh
@@ -444,7 +444,15 @@ timestamp ()
 newest_file () 
 { 
     # doesn't work if newline character is present anywhere
-    local dir="${1%/}"
-    local file_basename="$(ls -t "$1" | head -1)"
-    echo "${dir}/${file_basename}"
+    local dir="${1%/}"  # remove trailing directory separator
+    if [[ -z "$dir" ]]; then
+      local file_basename="$(ls -t | head -1)"
+    else
+      local file_basename="$(ls -t "$dir" | head -1)"
+    fi
+    if [[ -z "$dir" ]]; then
+      echo "${file_basename}"
+    else
+      echo "${dir}/${file_basename}"
+    fi
 }
